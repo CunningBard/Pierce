c749eb241cdedcdc9907d8573b41d234beae85f9
CunningBard <jhonjhonjumaday@gmail.com>
2022-06-23 20:05:12 +0800

    pierce

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pierce
 Pierce formerly known as YAPL, is a programming language inspired by the readability of python and the speed and features of rust
+
+# this is **DEV BRANCH** please see stable
+
+# check design.md for the design choices
--- src/ast_base.rs
+++ src/ast_base.rs
@@ -1,19 +1,47 @@
 use crate::lexer::Token;
 
-enum YaplTypes{
+pub enum PierceTypes {
+    // Heap
+    Int,
+    Str,
+    Bool,
+    Float,
+    List,
 
+    // Stack
+    Array,
+    String,
+
+    I8,
+    I16,
+    I32,
+    I64,
+    I128,
+
+    U8,
+    U16,
+    U32,
+    U64,
+    U128,
+
+    F32,
+    F64
 }
 
 pub struct MathExpression {
 
 }
 
-pub struct YaplVariable {
-    value: i128,
+pub struct Variable {
+    value: Vec<str>,
     name: String,
     tokens: Vec<Token>
 }
 
-pub enum YAPL {
-    Integer(YaplInt)
+pub struct FuncCall {
+
+}
+
+pub enum Pierce {
+    Variable(Variable)
 }
